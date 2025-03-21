7fa03cb60e6487145040cc7fc86e6145d181a5f9
nttu94507 <nttu94507@gmail.com>
2025-03-21 18:20:10 +0800

    rebuild nextJS 15

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-    // "@tailwindcss/postcss": {},
-  },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
